d55aaff24c41c3be57d16d576b354d1ff0164246
JanaIsCoding <jana.istrate@gmail.com>
2023-02-08 15:37:44 +0200

    Clear everything

--- src/modules/createTask.js
+++ src/modules/createTask.js
@@ -1,12 +1,43 @@
 export default class Task { 
     constructor(
-        title = '',
-        description ='',
-        dueDate = 0,
+        title = 'Default Title',
+        description ='Default Description',
+        dueDate = 'No Date Selected',
         priority = [Low, Medium, High]) {
         this.title = title
         this.description = description
         this.dueDate = dueDate
         this.priority = priority
+    }
+
+    setTitle(title){
+        this.title = title;
+    }
+
+    getTitle(){
+        return this.title;
+    }
+
+    setDate(dueDate){
+        this.dueDate = dueDate;
+    }
+
+    getDate(){
+        return this.dueDate;
+    }
+
+    setPriotity(priority){
+        this.priority = priority;
+    }
+
+    getPriority(){
+        return this.priority;
+    }
+
+    getDateFormatted(){
+        const day = this.dueDate.split('/')[0]
+        const month = this.dueDate.split('/')[1]
+        const year = this.dueDate.split('/')[2]
+        return `${month}/${day}/${year}`;
     }
 }
